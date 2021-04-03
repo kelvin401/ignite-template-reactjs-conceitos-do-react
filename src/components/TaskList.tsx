48150accba321fbe19462102ae8eb197d05e792f
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2021-04-02 22:29:56 -0300

    Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,29 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const idTask = Math.floor(Math.random() * 100);
+    const titleTask = newTaskTitle;
+
+    if (titleTask !== '') {
+      const body = [{ id: idTask, title: titleTask, isComplete: false }];
+      setTasks([...tasks, ...body])
+      setNewTaskTitle('');
+      return;
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const currentTask = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task);
+
+    setTasks(currentTask);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const removedTask = tasks.filter(task => task.id !== id);
+    setTasks(removedTask);
+    return
   }
 
   return (
